5655035d5490c0f0ed9e61ce20b3b9752f39849f
Mariia Hoienko <mariia-hoienko@on-running.com>
2025-03-13 16:39:51 +0100

    add button and img

--- components/CarType.vue
+++ components/CarType.vue
@@ -1,13 +1,11 @@
 <template>
     <div>
         <div v-for="carType in carTypes" :key="carType.id">
+            <img :src="carType.imgSrc" alt="Car Type Icon" :key="carType.id"/>
             <h2>{{ carType.name }}</h2>
             <p>{{ carType.description }}</p>
+            <button v-if="carType.buttonText">{{ carType.buttonText }}</button>
         </div>
-    </div>
-    <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
     </div>
 </template>
 
@@ -16,8 +14,10 @@ import { defineComponent, ref } from 'vue';
 
 interface CarType {
     id: number;
+    imgSrc: string;
     name: string;
     description: string;
+    buttonText?: string;
 }
 
 export default defineComponent({
@@ -25,18 +25,24 @@ export default defineComponent({
         const carTypes = ref<CarType[]>([
             {
                 id: 1,
+                imgSrc: '../assets/icon-sedans.svg',
                 name: 'Sedans',
                 description: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+                buttonText: 'Learn More',
             },
             {
                 id: 2,
+                imgSrc: '../assets/icon-suvs.svg',
                 name: 'SUVs',
                 description: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
+                buttonText: 'Learn More',
             },
             {
                 id: 3,
+                imgSrc: '../assets/icon-luxury.svg',
                 name: 'Luxury',
                 description: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+                buttonText: 'Learn More',
             },
         ]);
 
@@ -51,6 +57,5 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-    .attribution { font-size: 11px; text-align: center; }
-    .attribution a { color: hsl(228, 45%, 44%); }
+
 </style>
