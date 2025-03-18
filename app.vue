fc8592dd2cbf78a099b8cfa2e16d9b200e337d2a
Mariia Hoienko <mariia-hoienko@on-running.com>
2025-03-18 10:47:06 +0100

    set up prettier

--- app.vue
+++ app.vue
@@ -1,15 +1,28 @@
-<script>
-import CarType from '~/components/CarType.vue'
-export default {
-  components: {
-    CarType,
-  },
-};
+<script setup>
+ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/car-types">Car Types</router-link>
+  </nav>
   <div>
-    <CarType />
+    <router-view />
   </div>
 </template>
 
+<style scoped>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
