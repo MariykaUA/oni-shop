478d43c0135314907fb49133b8eed5711e7c98d4
Mariia Hoienko <mariia-hoienko@on-running.com>
2025-03-25 16:57:26 +0100

    add pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,38 @@
 <template>
   <div>
-    <slot />
+    <header>
+      <nav class="navigation">
+        <NuxtLink to="/"><img :src="'./logo.svg'" alt="On logo" class="logo-img"/></NuxtLink>
+        <NuxtLink to="/shoes">SHOES</NuxtLink>
+        <NuxtLink to="/accessoires">ACCESSOIRES</NuxtLink>
+        <NuxtLink to="/contact">CONTACT</NuxtLink>
+      </nav>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <p>footer</p>
+    </footer>
   </div>
 </template>
+
+<style lang="scss">
+header {
+  width: 100vh;
+  height: 70px;
+  flex-shrink: 0;
+}
+
+.navigation {
+  display: inline-flex;
+  align-items: center;
+  gap: 6px;
+}
+
+.logo-img {
+  width: 64px;
+  height: 64px;
+  flex-shrink: 0;
+}
+</style>
