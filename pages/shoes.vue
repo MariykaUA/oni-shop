2f6cc8cdeed0319926e370998fbb2e838fcf4275
Mariia Hoienko <mariia-hoienko@on-running.com>
2025-05-14 17:30:07 +0200

    fix styles

--- pages/shoes.vue
+++ pages/shoes.vue
@@ -17,6 +17,11 @@ import ProductList from '../components/ProductList.vue'
 @include until('small') {
   .shoes {
     width: 100%;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: -2rem;
   }
 }
 </style>
